6c3bbbc3c58b7d3a37e639036632bf224a781747
MorettiFrancisco <franmoretti02@hotmail.com>
2024-10-17 22:34:03 -0300

    Refactor header styling and add logo to home screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { Stack } from "expo-router";
+import { Image } from "react-native";
+
 
 const Layout = () => (
     <Stack>
-        <Stack.Screen name="HomeScreen/index" options={{ title: 'Plata Park' }} />
+        <Stack.Screen name="index" 
+        options={{
+                title: 'Plata Park',
+                headerStyle: {
+                    backgroundColor: '#CEECF5',
+                },
+                headerTitleStyle:{
+                    fontWeight: 'bold',
+                },
+                headerLeft: () => (
+                    <Image
+                        source={require('../assets/images/HeaderIcon.png')} 
+                        style={{ width: 40, height: 40, marginLeft: 10, marginRight: 20 }}
+                    />
+                ),
+            }}/>
     </Stack>
 );
 
