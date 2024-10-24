2b1f5fb66966f52589da85c3136ef00860ea9855
MorettiFrancisco <franmoretti02@hotmail.com>
2024-10-24 12:23:22 -0300

    zonas de estacionamiento medido, lunes a viernes y sabados

---
+++ constants/ParkingZones/LunesViernesSabado.ts
@@ -0,0 +1,116 @@
+const description = "lunes a viernes de 7:00 a 20:00 y sabados de 9:00 a 20:00";
+
+
+export const LunesViernesSabado= [
+    {
+        id: 1,
+        name: "47 y 6, diagonal 77 y 47, 51 y 6 a 51 y 4",
+        description: description,
+        coordinates: [
+            { latitude: -34.910016 , longitude: -57.948846 },
+            { latitude: -34.9100569 , longitude: -57.9488270 },
+            { latitude: -34.9125724 , longitude: -57.9459028 },
+            { latitude: -34.914377 , longitude: -57.947924 },
+        ]
+    },
+    {
+        id: 2,
+        name: "diagonal 80 y 45 a diagonal 80 y diagonal 77",
+        description: description,
+        coordinates: [
+            { latitude: -34.906673 , longitude: -57.949053 },
+            { latitude: -34.910016 , longitude: -57.948846 },
+        ]
+    },
+    {
+        id: 3,
+        name: "diagonal 80 y diagonal 77 a plaza Italia",
+        description: description,
+        coordinates: [
+            { latitude: -34.910760 , longitude: -57.955067 },
+            { latitude: -34.910713 , longitude: -57.954978 },
+        ]
+    },
+    {
+        id: 4,
+        name: "plaza Italia a plaza Moreno (diagonal 74)",
+        description: description,
+        coordinates: [
+            { latitude: -34.910713 , longitude: -57.954978 },
+            { latitude: -34.919642 , longitude: -57.954797 },
+        ]
+    },
+    {
+        id: 5,
+        name: "7 y 45 a 6 y 45",
+        description: description,
+        coordinates: [
+            { latitude: -34.911510 , longitude: -57.953986 },
+            { latitude: -34.910583 , longitude: -57.953055 },
+        ]
+    },
+    {
+        id: 6,
+        name: "6 y 45 a 6 y 47",
+        description: description,
+        coordinates: [
+            { latitude: -34.910591 , longitude: -57.953058 },
+            { latitude: -34.912115 , longitude: -57.951069 },
+        ]
+    },
+    {
+        id: 7,
+        name: "5 y 46 a 5 y 47",
+        description: description,
+        coordinates: [
+            { latitude: -34.910301 , longitude: -57.951253 },
+            { latitude: -34.911210 , longitude: -57.950012 },
+        ]
+    },
+    {
+        id: 8,
+        name: "7 y 46 a 5 y 46",
+        description: description,
+        coordinates: [
+            { latitude: -34.912332 , longitude: -57.952984 },
+            { latitude: -34.910281 , longitude: -57.950769 },
+        ]
+    },
+    {
+        id: 9,
+        name: "7 y 47 a 6 y 47",
+        description: description,
+        coordinates: [
+            { latitude: -34.912117 , longitude: -57.951091 },
+            { latitude: -34.913026 , longitude: -57.952080 },
+        ]
+    },
+    {
+        id: 10,
+        name: "6 y 48 a 7 y 48",
+        description: description,
+        coordinates: [
+            { latitude: -34.913653 , longitude: -57.951164 },
+            { latitude: -34.912751 , longitude: -57.950181 },
+        ]
+    },
+    {
+        id: 11,
+        name: "6 y 49 a 7 y 49",
+        description: description,
+        coordinates: [
+            { latitude: -34.913345 , longitude: -57.949376 },
+            { latitude: -34.914249 , longitude: -57.950362 },
+        ]
+    },
+    {
+        id : 12,
+        name: "7 y 50 a 6 y 50",
+        description: description,
+        coordinates: [
+            { latitude: -34.914745 , longitude: -57.949611 },
+            { latitude: -34.913861 , longitude: -57.948646 },
+        ]
+    },        
+    //falta la parte de abajo y 7 para plaza rocha
+];
