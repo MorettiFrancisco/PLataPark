9d4430db43da2b5b7557595a4cbcdde0ddceeca8
Maximo <maxipreneste@gmail.com>
2024-11-15 17:09:03 -0300

    malditos intents

--- app/LuncherSEM.tsx
+++ app/LuncherSEM.tsx
@@ -1,16 +1,21 @@
 import { StyleSheet, View, TouchableOpacity, Image, Alert, Text } from 'react-native';
 import {startActivityAsync} from 'expo-intent-launcher';
+import * as IntentLauncher from 'expo-intent-launcher';
 export default function LuncherSEM() {
-    const SEM='com.whatsapp.HomeActivity'
-    
+    const SEM='ar.edu.unlp.semmobile.laplata'
+    const i = {
+        //action: 'android.intent.action.MAIN',
+        category: 'android.intent.category.LAUNCHER',
+        packageName: SEM
+      };
     const intent=async ()=>{
         Alert.alert('Sem','Abriendo el sem')
-         startActivityAsync(SEM);
+        startActivityAsync('android.intent.action.VIEW',i);
     }
 
 
     return (<>
-            <TouchableOpacity style={{}} onPress={ intent}>
+            <TouchableOpacity style={{}} onPress={intent}>
                 <Text>
                     SEM
                 </Text>
