80d23bcb8dfea62fee53a65cc265b15863bdfd2a
Maximo <maxipreneste@gmail.com>
2024-11-15 21:37:12 -0300

    linkeo

--- app/LuncherSEM.tsx
+++ app/LuncherSEM.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, View, TouchableOpacity, Image, Alert, Text } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Image, Alert, Text, Platform } from 'react-native';
 import {startActivityAsync} from 'expo-intent-launcher';
 import * as IntentLauncher from 'expo-intent-launcher';
+import { openURL } from 'expo-linking';
 export default function LuncherSEM() {
     const SEM='ar.edu.unlp.semmobile.laplata'
     const i = {
@@ -9,14 +10,24 @@ export default function LuncherSEM() {
         packageName: SEM
       };
     const intent=async ()=>{
-        Alert.alert('Sem','Abriendo el sem')
-        startActivityAsync('android.intent.action.VIEW',i);
+        try {
+            if (Platform.OS==='android') {
+                openURL("https://play.google.com/store/apps/details?id=ar.edu.unlp.semmobile.laplata&hl=es_AR&gl=US")
+            }else if (Platform.OS==='ios'){
+                openURL("https://apps.apple.com/ar/app/sem-mobile/id1387705895")
+            }
+        } catch (error) {
+            
+        }
     }
 
 
     return (<>
-            <TouchableOpacity style={{}} onPress={intent}>
-                <Text>
+            <TouchableOpacity style={{backgroundColor:'#CEECF5',height:40}} onPress={intent}>
+                <Text style={{position:'absolute',
+                padding:20,
+                paddingLeft:175
+                }}>
                     SEM
                 </Text>
             </TouchableOpacity>
